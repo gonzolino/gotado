89fbd798ade93878ab2c9a27d8f77d9e1555b4cb
Daniel Gonzalez <daniel@gonzalez-nothnagel.de>
2021-07-04 23:42:36 +0200

    docs: add new misc home info example
    The example is initialized with code to read current weather
    information, but further information about the home will be added to
    this example.

---
+++ examples/mischomeinfo/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gonzolino/gotado"
+)
+
+const (
+	clientID     = "tado-web-app"
+	clientSecret = "wZaRN7rpjn3FoNyF5IFuxg9uMzYJcvOoQ8QWiIqS3hfk6gLhVlG57j5YNoZL2Rtc"
+)
+
+func main() {
+	// Get credentials from env vars
+	username, ok := os.LookupEnv("TADO_USERNAME")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Variable TADO_USERNAME not set\n")
+		os.Exit(1)
+	}
+	password, ok := os.LookupEnv("TADO_PASSWORD")
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Variable TADO_PASSWORD not set\n")
+		os.Exit(1)
+	}
+
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s homeName\n", os.Args[0])
+		os.Exit(1)
+	}
+	homeName := os.Args[1]
+
+	ctx := context.Background()
+
+	// Create authenticated tado° client
+	client := gotado.NewClient(clientID, clientSecret).WithTimeout(5 * time.Second)
+	client, err := client.WithCredentials(ctx, username, password)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Authentication failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	user, err := gotado.GetMe(client)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to get user info: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Find the home to control
+	var home *gotado.UserHome
+	for _, h := range user.Homes {
+		if h.Name == homeName {
+			home = &h
+			break
+		}
+	}
+	if home == nil {
+		fmt.Fprintf(os.Stderr, "Home '%s' not found\n", homeName)
+		os.Exit(1)
+	}
+
+	// Get weather
+	weather, err := gotado.GetWeather(client, home)
+	fmt.Println("Weather:")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to get weather: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Solar Intensity: %.2f\n", weather.SolarIntensity.Percentage)
+	fmt.Printf("Outside Temperature: %.2f °C\n", weather.OutsideTemperature.Celsius)
+	fmt.Printf("Weather State: %s\n", weather.WeatherState.Value)
+}
